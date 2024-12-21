4ff425f72a970ed5fa8ddd913024065ce2c2299f
devmeeple <devmeeple@gmail.com>
2024-12-22 05:58:27 +0900

    코틀린 프로그램 실행

---
+++ src/main/kotlin/HelloKotlin.kt
@@ -0,0 +1,5 @@
+package tobyspring
+
+fun main() {
+    println("Hello Kotlin")
+}
---
+++ src/main/kotlin/HelloKotlin2.kt
@@ -0,0 +1,7 @@
+package tobyspring
+
+fun main() {
+    println("Hello Kotlin1")
+    println("Hello Kotlin2")
+    println("Hello Kotlin3")
+}
