b382bc5052b5290f5c61bfee2d5467d3ecb3bc62
devmeeple <devmeeple@gmail.com>
2024-12-26 00:09:03 +0900

    [문제 2] 실수의 평균

--- src/test/kotlin/operator/OperatorTest.kt
+++ src/test/kotlin/operator/OperatorTest.kt
@@ -18,4 +18,17 @@ class OperatorTest {
         assertThat(average).isEqualTo(20)
     }
 
+    @Test
+    fun `실수의 평균을 계산한다`() {
+        val num1 = 1.5
+        val num2 = 2.5
+        val num3 = 3.5
+        val sum = num1 + num2 + num3
+
+        val average = sum / 3;
+
+        assertThat(sum).isEqualTo(7.5)
+        assertThat(average).isEqualTo(2.5)
+    }
+
 }
