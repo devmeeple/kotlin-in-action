3662c7928ad1a6e4cb9c3eeb0bd162e91297a9b0
devmeeple <devmeeple@gmail.com>
2024-12-26 15:25:34 +0900

    [2-1] 문제 3. Null safety

--- src/test/kotlin/guide/ch02/MemberServiceTest.kt
+++ src/test/kotlin/guide/ch02/MemberServiceTest.kt
@@ -20,4 +20,19 @@ class MemberServiceTest {
 
         assertThat(id).isEqualTo(1)
     }
+
+    @Test
+    fun `이메일을 입력하지 않으면 기본값을 할당한다`() {
+        val email: String? = null
+
+        val result = signUp(email)
+
+        assertThat(result).isEqualTo("unknown@example.com")
+    }
+}
+
+// TODO 파라미터 기본값을 할당할 수 없을까, Elvis 연산자로 문제를 해결해야 할까?
+fun signUp(email: String?): String {
+    val defaultEmail = "unknown@example.com"
+    return email ?: defaultEmail
 }
