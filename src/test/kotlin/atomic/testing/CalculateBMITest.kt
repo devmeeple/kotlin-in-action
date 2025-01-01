9c8276affc56a503abea8bb88b6bad7456c478df
장태근 <devmeeple@gmail.com>
2025-01-01 14:40:51 +0900

    [22-Testing] BMI 구하기(저체중)

--- src/test/kotlin/atomic/testing/CalculateBMITest.kt
+++ src/test/kotlin/atomic/testing/CalculateBMITest.kt
@@ -5,13 +5,29 @@ import io.kotest.matchers.shouldBe
 
 class CalculateBMITest : FunSpec({
     test("체질량지수 18.5 이상 23미만은 정상 체중이다") {
-        val height = 180
-        val weight = 75
+        val height = 170
+        val weight = 65
 
         val result = calculateBMI(height, weight)
 
         result shouldBe "정상 체중입니다"
     }
+
+    test("체질량지수 18.5 미만은 저체중이다") {
+        val height = 180
+        val weight = 50
+
+        val result = calculateBMI(height, weight)
+
+        result shouldBe "저체중입니다"
+    }
 })
 
-fun calculateBMI(height: Int, weight: Int) = "정상 체중입니다"
+fun calculateBMI(height: Int, weight: Int): String {
+    val meter = height / 100.0
+    val bmi = weight / (meter * meter)
+
+    return if (bmi < 18.5) "저체중입니다"
+    else if (bmi < 23) "정상 체중입니다"
+    else "과체중입니다"
+}
