30216629d1aa13a54e66f5749eef55fbaff45f4a
장태근 <devmeeple@gmail.com>
2025-01-01 17:01:08 +0900

    [17] 멤버 함수(클래스에 소속된 함수)

--- src/test/kotlin/atomic/classes/ClassTest.kt
+++ src/test/kotlin/atomic/classes/ClassTest.kt
@@ -1,6 +1,7 @@
 package atomic.classes
 
 import io.kotest.core.spec.style.FunSpec
+import io.kotest.matchers.shouldBe
 import io.kotest.matchers.shouldNotBe
 import io.kotest.matchers.types.shouldBeInstanceOf
 
@@ -11,6 +12,19 @@ class ClassTest : FunSpec({
         book shouldNotBe null
         book.shouldBeInstanceOf<EmptyBook>()
     }
+
+    test("클래스에 소속된 함수는 멤버 함수다") {
+        val title = "아토믹 코틀린"
+        val book = Book(title)
+
+        val result = book.introduce()
+
+        result shouldBe "반갑습니다. 아토믹 코틀린"
+    }
 })
 
 class EmptyBook
+
+class Book(private val title: String) {
+    fun introduce() = "반갑습니다. $title"
+}
