32e8e03d7eaab2c5a15a117c004e192abf47aab3
장태근 <devmeeple@gmail.com>
2025-01-05 00:27:51 +0900

    1. WelcomeToHell 요약
    - val vs. var
    - String templates
    - update test name

--- src/test/kotlin/tour/WelcomeToHellTest.kt
+++ src/test/kotlin/tour/WelcomeToHellTest.kt
@@ -4,7 +4,7 @@ import io.kotest.core.spec.style.FunSpec
 import io.kotest.matchers.shouldBe
 
 class WelcomeToHellTest : FunSpec({
-    test("지옥에 오신걸 환영합니다.") {
+    test("지옥에 오신 걸 환영합니다.") {
         val message = "Welcome to Hell!"
 
         message shouldBe "Welcome to Hell!"
@@ -13,8 +13,8 @@ class WelcomeToHellTest : FunSpec({
     test("문자열 템플릿을 사용하여 값을 출력한다.") {
         val problems = 2
 
-        val result = "오늘 발생한 문제는 ${problems}건 입니다."
+        val result = "오늘 발생한 문제는 ${problems}건입니다."
 
-        result shouldBe "오늘 발생한 문제는 2건 입니다."
+        result shouldBe "오늘 발생한 문제는 2건입니다."
     }
 })
