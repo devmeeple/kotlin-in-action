a3c19c61251a95dc44beac8f7b7c35c4d988f195
장태근 <devmeeple@gmail.com>
2025-01-05 10:05:26 +0900

    [3-1] List 확장 함수(last)

--- src/test/kotlin/tour/collections/ListTest.kt
+++ src/test/kotlin/tour/collections/ListTest.kt
@@ -11,4 +11,12 @@ class ListTest : FunSpec({
 
         result shouldBe 3
     }
+
+    test("단체 채팅방에 마지막으로 입장한 참가자를 조회한다") {
+        val members = listOf("조영호", "로이 오셔로브", "블라디미르 코리코프")
+
+        val result = members.last()
+
+        result shouldBe "블라디미르 코리코프"
+    }
 })
