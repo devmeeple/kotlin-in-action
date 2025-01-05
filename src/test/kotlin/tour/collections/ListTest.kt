131af671009ee35cd164f5d38c5b24962e3f2374
장태근 <devmeeple@gmail.com>
2025-01-05 10:08:41 +0900

    [3-1] List 확장 함수(first) - 첫 번째 참가자 조회

--- src/test/kotlin/tour/collections/ListTest.kt
+++ src/test/kotlin/tour/collections/ListTest.kt
@@ -19,4 +19,12 @@ class ListTest : FunSpec({
 
         result shouldBe "블라디미르 코리코프"
     }
+
+    test("단체 채팅방에 첫번째 입장한 참가자를 조회한다") {
+        val members = listOf("조영호", "로이 오셔로브", "블라디미르 코리코프")
+
+        val result = members.first()
+
+        result shouldBe "조영호"
+    }
 })
