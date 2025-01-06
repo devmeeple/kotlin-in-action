3524f0a345cc9d61630e5538ea1b48ee63d3f49c
장태근 <devmeeple@gmail.com>
2025-01-06 21:16:09 +0900

    [4-1] 테스트 개선 1안, 고정된 값 사용하기

--- src/test/kotlin/tour/controlflow/ConditionalExpressionTest.kt
+++ src/test/kotlin/tour/controlflow/ConditionalExpressionTest.kt
@@ -2,13 +2,15 @@ package tour.controlflow
 
 import io.kotest.core.spec.style.FunSpec
 import io.kotest.matchers.shouldBe
-import kotlin.random.Random
 
 class ConditionalExpressionTest : FunSpec({
     test("주사위를 2개를 던져 같은 숫자가 나오면 승리한다") {
         // TODO: 2025.01.06 제어할 수 없는 코드를 밀어내자. Random은 테스트하기 어렵다. 그런데 왜 항상 성공할까?
-        val firstResult = Random.nextInt(6)
-        val secondResult = Random.nextInt(6)
+//        val firstResult = Random.nextInt(6)
+//        val secondResult = Random.nextInt(6)
+        // TODO: 2025.01.06 첫 번째 발자국, 랜덤이 아닌 고정된 값을 사용해서 문제를 해결한다.
+        val firstResult = 3
+        val secondResult = 3
 
         val result = compareNumber(firstResult, secondResult)
 
