2cdbcffc6b3aad551de615cf2cfabb3854c0a019
장태근 <devmeeple@gmail.com>
2025-01-07 23:54:23 +0900

    [4-2] 1. PizzaSliceCounterTest do-while

--- src/test/kotlin/tour/collections/PizzaSliceCounterTest.kt
+++ src/test/kotlin/tour/collections/PizzaSliceCounterTest.kt
@@ -9,6 +9,12 @@ class PizzaSliceCounterTest : FunSpec({
 
         result shouldBe 8
     }
+
+    test("[do-while] 피자가 8조각이면 프로그램을 종료한다") {
+        val result = countSlicesUsingDoWhile()
+
+        result shouldBe 8
+    }
 })
 
 const val PIZZA_MAX_SLICES = 8
@@ -20,4 +26,13 @@ fun countSlicesUsingWhile(): Int {
     }
 
     return slices
+}
+
+fun countSlicesUsingDoWhile(): Int {
+    var slices = 0
+    do {
+        slices++
+    } while (slices < PIZZA_MAX_SLICES)
+    return slices
+
 }
