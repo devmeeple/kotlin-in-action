1ec1f80d6859cf275d967616df21187ee91ac8ab
장태근 <devmeeple@gmail.com>
2025-01-07 16:21:57 +0900

    4. [4-1] 2. JoyStickTest 버튼 케이스 추가와 엣지 수정

--- src/test/kotlin/tour/controlflow/JoyStickTest.kt
+++ src/test/kotlin/tour/controlflow/JoyStickTest.kt
@@ -6,14 +6,14 @@ import io.kotest.matchers.shouldBe
 
 class JoyStickTest : FunSpec({
     // TODO: 2025.01.07 엣지(Edge) 케이스
-    test("없는 버튼을 클릭하면 에러가 발생한다") {
-        val button = "X"
+    test("[Edge] 없는 버튼을 클릭하면 에러가 발생한다") {
+        val button = "asdf"
 
         val result = shouldThrow<IllegalArgumentException> {
             showActionMessage(button)
         }
 
-        result.message shouldBe "[X] 버튼을 찾을 수 없습니다"
+        result.message shouldBe "[asdf] 버튼을 찾을 수 없습니다"
     }
 
     test("A 버튼을 클릭하면 'YES' 메시지를 출력한다") {
@@ -23,11 +23,38 @@ class JoyStickTest : FunSpec({
 
         result shouldBe "YES"
     }
+
+    test("B 버튼을 클릭하면 'NO' 메시지를 출력한다") {
+        val button = "B"
+
+        val result = showActionMessage(button)
+
+        result shouldBe "NO"
+    }
+
+    test("X 버튼을 클릭하면 'MENU' 메시지를 출력한다") {
+        val button = "X"
+
+        val result = showActionMessage(button)
+
+        result shouldBe "MENU"
+    }
+
+    test("Y 버튼을 클릭하면 'NOTHING' 메시지를 출력한다") {
+        val button = "Y"
+
+        val result = showActionMessage(button)
+
+        result shouldBe "NOTHING"
+    }
 })
 
 fun showActionMessage(button: String): String {
     return when (button) {
         "A" -> "YES"
+        "B" -> "NO"
+        "X" -> "MENU"
+        "Y" -> "NOTHING"
         else -> throw IllegalArgumentException("[${button}] 버튼을 찾을 수 없습니다")
     }
 }
