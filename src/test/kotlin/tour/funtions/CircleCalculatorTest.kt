cfef419d6e4d332f57784a1aab887ffe23e36587
장태근 <devmeeple@gmail.com>
2025-01-08 21:01:21 +0900

    5. Functions 연습 1 원의 넓이 계산하기

---
+++ src/test/kotlin/tour/funtions/CircleCalculatorTest.kt
@@ -0,0 +1,24 @@
+package tour.funtions
+
+import io.kotest.core.spec.style.BehaviorSpec
+import io.kotest.matchers.shouldBe
+import java.math.BigDecimal
+import java.math.RoundingMode
+import kotlin.math.PI
+
+class CircleCalculatorTest : BehaviorSpec({
+    Given("radius") {
+        val radius = 2
+        When("PI와 반지름의 제곱을 곱한") {
+            val result = circleArea(radius)
+            Then("원의 넓이를 반환한다") {
+                result shouldBe BigDecimal("12.57")
+            }
+        }
+    }
+})
+
+fun circleArea(radius: Int): BigDecimal {
+    val area = PI * radius * radius
+    return BigDecimal(area).setScale(2, RoundingMode.HALF_UP)
+}
