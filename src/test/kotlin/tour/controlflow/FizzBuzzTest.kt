e660bf59c0df25f41a4411427033d75dfdd781a8
장태근 <devmeeple@gmail.com>
2025-01-08 17:03:11 +0900

    [4-2] 3. fizzbuzz 3과 5의 공배수

--- src/test/kotlin/tour/controlflow/FizzBuzzTest.kt
+++ src/test/kotlin/tour/controlflow/FizzBuzzTest.kt
@@ -16,13 +16,20 @@ class FizzBuzzTest : BehaviorSpec({
                 fizzBuzz(5) shouldBe "buzz"
             }
         }
+
+        When("[공배수] 3과 5로 나누어지는 숫자는") {
+            Then("[fizzbuzz]라는 단어로 변경한다") {
+                fizzBuzz(15) shouldBe "fizzbuzz"
+            }
+        }
     }
 })
 
 fun fizzBuzz(number: Int): String {
     return when {
-        number % 3 == 0 -> "fizz"
+        number % 15 == 0 -> "fizzbuzz"
         number % 5 == 0 -> "buzz"
+        number % 3 == 0 -> "fizz"
         else -> number.toString()
     }
 }
