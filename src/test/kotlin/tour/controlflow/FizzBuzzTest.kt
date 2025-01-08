d5f22aba374fceed7e817ecf0c1642901e95d77c
장태근 <devmeeple@gmail.com>
2025-01-08 17:19:31 +0900

    [4-2] 3. fizzbuzz Enum classes 리팩터링
    - companion object
    - entries, firstOrNull

--- src/test/kotlin/tour/controlflow/FizzBuzzTest.kt
+++ src/test/kotlin/tour/controlflow/FizzBuzzTest.kt
@@ -25,11 +25,32 @@ class FizzBuzzTest : BehaviorSpec({
     }
 })
 
+// TODO: 2025.01.08 `Enum`을 사용해 추상화 리팩터링
 fun fizzBuzz(number: Int): String {
-    return when {
-        number % 15 == 0 -> "fizzbuzz"
-        number % 5 == 0 -> "buzz"
-        number % 3 == 0 -> "fizz"
-        else -> number.toString()
+    return FizzBuzzRule.evaluate(number)
+}
+
+enum class FizzBuzzRule(val divisor: Int, val word: String) {
+    FIZZBUZZ(15, "fizzbuzz"),
+    BUZZ(5, "buzz"),
+    FIZZ(3, "fizz");
+
+    // TODO: 2025.01.08 `Enum`에 직접 접근하는 정적 메서드
+    /**
+     * What is companion object?
+     * https://medium.com/@riztech.dev/understanding-companion-objects-in-kotlin-a93f1a5880a7#:~:text=In%20Kotlin%2C%20a%20companion%20object,on%20instances%20of%20the%20class.
+     */
+    companion object {
+        /**
+         * 입력받은 숫자(number)를 가장 먼저 만족하는 FizzBuzzRule `word`를 반환한다.
+         * entries: `Enum`의 모든 값을 순회한다.
+         *          https://www.jetbrains.com/help/inspectopedia/EnumValuesSoftDeprecate.html
+         * firstOrNull: 조건을 만족하는 첫 번째 값을 찾는다. 없으면 `null`을 반환한다.
+         * ?.word: 조건에 만족하는 값이 있으면 `Enum`의 `word` 값을 가져온다. 없으면 `null`
+         * ?: `number.toString()`: `null`이면 `number`를 문자열로 반환한다.
+         */
+        fun evaluate(number: Int): String {
+            return entries.firstOrNull() { number % it.divisor == 0 }?.word ?: number.toString()
+        }
     }
 }
