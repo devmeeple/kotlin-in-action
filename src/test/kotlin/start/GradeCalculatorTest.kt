23c394c63f6e05c9b04e1b4ebdcec9efad5e90a4
장태근 <devmeeple@gmail.com>
2025-01-15 23:43:50 +0900

    D 학점 추가

--- src/test/kotlin/start/GradeCalculatorTest.kt
+++ src/test/kotlin/start/GradeCalculatorTest.kt
@@ -29,6 +29,14 @@ class GradeCalculatorTest : BehaviorSpec({
             }
         }
 
+        When("60점 이상 70점 미만은") {
+            val result = calculate(69)
+
+            Then("학점은 'D'입니다") {
+                result shouldBe "학점은 D입니다"
+            }
+        }
+
     }
 })
 
@@ -37,6 +45,7 @@ fun calculate(score: Int): String {
         in 90..100 -> "학점은 A입니다"
         in 80..89 -> "학점은 B입니다"
         in 70..79 -> "학점은 C입니다"
+        in 60..69 -> "학점은 D입니다"
         else -> "학점은 F입니다."
     }
 }
