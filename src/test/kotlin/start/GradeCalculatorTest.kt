f1fd0ee094a12bdc713e874b957df85877bcdb40
장태근 <devmeeple@gmail.com>
2025-01-15 23:41:39 +0900

    C 학점 추가

--- src/test/kotlin/start/GradeCalculatorTest.kt
+++ src/test/kotlin/start/GradeCalculatorTest.kt
@@ -21,6 +21,14 @@ class GradeCalculatorTest : BehaviorSpec({
             }
         }
 
+        When("70점 이상 80점 미만은") {
+            val result = calculate(79)
+
+            Then("학점은 'C'입니다") {
+                result shouldBe "학점은 C입니다"
+            }
+        }
+
     }
 })
 
@@ -28,6 +36,7 @@ fun calculate(score: Int): String {
     return when (score) {
         in 90..100 -> "학점은 A입니다"
         in 80..89 -> "학점은 B입니다"
+        in 70..79 -> "학점은 C입니다"
         else -> "학점은 F입니다."
     }
 }
