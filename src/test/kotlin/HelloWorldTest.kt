cf7cfa0c5b24d03c3c536ab825551e57098b8f34
장태근 <devmeeple@gmail.com>
2025-01-22 23:55:48 +0900

    Hello World! Add Single-expression function

--- src/test/kotlin/HelloWorldTest.kt
+++ src/test/kotlin/HelloWorldTest.kt
@@ -3,9 +3,29 @@ import io.kotest.matchers.shouldBe
 
 class HelloWorldTest : FunSpec({
 
-    test("안녕하세요. Kotest") {
-        val message = "안녕하세요. Kotest"
+    val message = "안녕하세요. Kotest"
 
-        message shouldBe "안녕하세요. Kotest"
+    test("안녕하세요. Kotest") {
+
+        val result = hello(message)
+
+        result shouldBe "안녕하세요. Kotest"
     }
+
+    test("[Single] 안녕하세요. Kotest") {
+        val result = helloSingle(message)
+
+        result shouldBe "안녕하세요. Kotest"
+    }
+
+
 })
+
+fun hello(message: String): String {
+    return message
+}
+
+fun helloSingle(message: String) = message
+
+
+
