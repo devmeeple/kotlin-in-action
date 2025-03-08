079d6978589053b1bb4bff99e5c64bfecae4ac70
devmeeple <devmeeple@gmail.com>
2025-03-08 23:37:46 +0900

    [3-1] 컬렉션 만들기 (1)

---
+++ src/main/kotlin/ch03/CreatingCollectionsInKotlin1.kt
@@ -0,0 +1,12 @@
+package ch03
+
+fun main() {
+    val strings = listOf("first", "second", "fourteenth")
+
+    strings.last()
+
+    println(strings.shuffled())
+
+    val numbers = setOf(1, 14, 2)
+    println(numbers.sum())
+}
