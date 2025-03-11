0ce04a8cfbb97d67c3a7f98f4936cb0c7994a13e
devmeeple <devmeeple@gmail.com>
2025-03-11 15:47:27 +0900

    [3-6] 코드 중복을 보여주는 예제

---
+++ src/main/kotlin/ch03/ValidateUser.kt
@@ -0,0 +1,22 @@
+package ch03
+
+class User(val id: Int, val name: String, val address: String)
+
+fun saveUser(user: User) {
+    if (user.name.isEmpty()) {
+        throw IllegalArgumentException(
+            "Can't save user ${user.id}: empty Name")
+
+    }
+
+    if (user.address.isEmpty()) {
+        throw IllegalArgumentException(
+            "Can't save user ${user.id}: empty Address")
+    }
+
+    // user를 데이터베이스에 저장한다.
+}
+
+fun main() {
+    saveUser(User(1, "", ""))
+}
