b2b0ef89cb12c74af5882872e78795ea13b794d7
devmeeple <devmeeple@gmail.com>
2025-03-13 23:07:52 +0900

    [4-3] 객체의 동등성

---
+++ src/main/kotlin/ch04/ObjectEqualityEquals.kt
@@ -0,0 +1,13 @@
+package ch04.ObjectEqualityEquals
+
+class Customer(
+    val name: String,
+    val postalCode: Int
+)
+
+fun main() {
+    val customer1 = Customer("Alice", 342562)
+    val customer2 = Customer("Alice", 342562)
+    
+    println(customer1 == customer2)
+}
