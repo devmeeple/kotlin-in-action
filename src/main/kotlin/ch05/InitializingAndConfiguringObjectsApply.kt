4e05aa45c42b2cbd2fef9100e631fb34ba4a9e18
devmeeple <devmeeple@gmail.com>
2025-03-14 14:54:31 +0900

    [5-4] apply를 사용해 알파벳 만들기

---
+++ src/main/kotlin/ch05/InitializingAndConfiguringObjectsApply.kt
@@ -0,0 +1,12 @@
+package ch05.InitializingAndConfiguringObjectsApply
+
+fun alphabet() = StringBuilder().apply {
+    for (letter in 'A'..'Z') {
+        append(letter)
+    }
+    append("\nNow I know the alphabet!")
+}.toString()
+
+fun main() {
+    println(alphabet())
+}
