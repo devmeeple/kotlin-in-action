d7ecaf0570da8ee1bb1c33cf1a6691a583f622ea
devmeeple <devmeeple@gmail.com>
2025-03-14 14:19:31 +0900

    [5-1] 이름 붙인 인자를 사용해 람다 넘기기

---
+++ src/main/kotlin/ch05/SyntaxForLambdaExpressions2.kt
@@ -0,0 +1,15 @@
+package ch05.SyntaxForLambdaExpressions2
+
+data class Person(
+    val name: String,
+    val age: Int
+)
+
+fun main() {
+    val people = listOf(Person("Alice", 29), Person("Bob", 31))
+    val names = people.joinToString(
+        separator = " ",
+        transform = { p: Person -> p.name }
+    )
+    println(names)
+}
