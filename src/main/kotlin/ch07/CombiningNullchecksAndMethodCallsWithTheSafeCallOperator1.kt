0dcf59bd27891a461d3b1cfc7277b5df858aea39
devmeeple <devmeeple@gmail.com>
2025-03-18 02:24:19 +0900

    [7-4] 널이 될 수 있는 프로퍼티를 다루기 위해 안전한 호출 사용하기

---
+++ src/main/kotlin/ch07/CombiningNullchecksAndMethodCallsWithTheSafeCallOperator1.kt
@@ -0,0 +1,15 @@
+package ch07.CombiningNullchecksAndMethodCallsWithTheSafeCallOperator1
+
+class Employee(
+    val name: String,
+    val manager: Employee?
+)
+
+fun managerName(employee: Employee): String? = employee.manager?.name
+
+fun main() {
+    val ceo = Employee("Da Boss", null)
+    val developer = Employee("Bob Smith", ceo)
+    println(managerName(developer))
+    println(managerName(ceo))
+}
