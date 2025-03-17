08ecc52bb3332c66c2deb952dde1f2c964aaf767
devmeeple <devmeeple@gmail.com>
2025-03-18 02:57:03 +0900

    [7-9] 지연 초기화하는 프로퍼티 사용하기

---
+++ src/main/kotlin/ch07/NonnullTypesWithoutImmediateInitializationLateinitializedProperties1.kt
@@ -0,0 +1,25 @@
+package ch07.NonnullTypesWithoutImmediateInitializationLateInitialization1
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.BeforeAll
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.TestInstance
+
+class MyService {
+    fun performAction(): String = "Action Done!"
+}
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+class MyTest {
+    private lateinit var myService: MyService
+
+    @BeforeAll
+    fun setUp() {
+        myService = MyService()
+    }
+
+    @Test
+    fun testAction() {
+        assertEquals("Action Done!", myService.performAction())
+    }
+}
