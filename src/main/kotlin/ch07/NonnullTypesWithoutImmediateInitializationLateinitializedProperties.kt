b9c3c9e4aa91fd20d12674bed5b951e08802012e
devmeeple <devmeeple@gmail.com>
2025-03-18 02:55:03 +0900

    [7-9] 널 아님 단언을 사용해 널이 될 수 있는 프로퍼티 접근하기

---
+++ src/main/kotlin/ch07/NonnullTypesWithoutImmediateInitializationLateinitializedProperties.kt
@@ -0,0 +1,25 @@
+package ch07.NonnullTypesWithoutImmediateInitializationLateInitialization
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.BeforeAll
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.TestInstance
+
+class MyService {
+    fun performAction(): String = "Action Done!"
+}
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+class MyTest {
+    private var myService: MyService? = null
+
+    @BeforeAll
+    fun setUp() {
+        myService = MyService()
+    }
+
+    @Test
+    fun testAction() {
+        assertEquals("Action Done!", myService!!.performAction())
+    }
+}
