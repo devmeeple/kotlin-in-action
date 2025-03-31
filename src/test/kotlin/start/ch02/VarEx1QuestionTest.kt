6a886b0f80ce19d1433bcb436f3dce167df359b4
devmeeple <devmeeple@gmail.com>
2025-03-31 09:29:48 +0900

    [3-7] 문제와 풀이 (1) 두 수의 차

--- src/test/kotlin/start/ch02/VarEx1QuestionTest.kt
+++ src/test/kotlin/start/ch02/VarEx1QuestionTest.kt
@@ -7,10 +7,18 @@ class VarEx1QuestionTest : BehaviorSpec({
     Given("두 숫자가 주어졌을 때") {
         val num1 = 4
         val num2 = 3
+
         When("두 숫자를 더하면") {
             val result = num1 + num2
             Then("합을 반환한다") {
                 result shouldBe 7
+            }
+        }
+
+        When("두 숫자를 빼면") {
+            val result = num1 - num2
+            Then("차를 반환한다") {
+                result shouldBe 1
             }
         }
 
