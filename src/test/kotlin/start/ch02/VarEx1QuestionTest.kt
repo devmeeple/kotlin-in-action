3f110fc499f72eeb2721759078cb0c2fe6fa0fe6
devmeeple <devmeeple@gmail.com>
2025-03-31 09:28:31 +0900

    [3-7] 문제와 풀이 (1) 두 수의 곱

--- src/test/kotlin/start/ch02/VarEx1QuestionTest.kt
+++ src/test/kotlin/start/ch02/VarEx1QuestionTest.kt
@@ -7,12 +7,17 @@ class VarEx1QuestionTest : BehaviorSpec({
     Given("두 숫자가 주어졌을 때") {
         val num1 = 4
         val num2 = 3
-
         When("두 숫자를 더하면") {
             val result = num1 + num2
-
             Then("합을 반환한다") {
                 result shouldBe 7
+            }
+        }
+
+        When("두 숫자를 곱하면") {
+            val result = num1 * num2
+            Then("곱을 반환한다") {
+                result shouldBe 12
             }
         }
     }
