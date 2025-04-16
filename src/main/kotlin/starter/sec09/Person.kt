2d7c9887c72b8ac39245226c7cfdcd66cec96f60
TaeGeun Jang <devmeeple@gmail.com>
2025-04-16 23:57:09 +0900

    [4-1] 코틀린에서 클래스를 다루는 방법

---
+++ src/main/kotlin/starter/sec09/Person.kt
@@ -0,0 +1,27 @@
+package starter.sec09
+
+/**
+ * primary constructor
+ * secondary constructor
+ * default parameter
+ * 정적 팩토리 메서드
+ * custom getter, setter
+ *  객체의 속성이라면 custom getter 권장, 그렇지 않다면 함수
+ * backing field
+ */
+
+class Person(
+    val name: String = "장태근",
+    var age: Int = 1,
+) {
+
+    init {
+        if (age <= 0) {
+            throw IllegalArgumentException("나이는 ${age}일 수 없습니다")
+        }
+        println("초기화 블록")
+    }
+
+    val isAdult: Boolean
+        get() = age >= 20
+}
