491a283c88d175e4eb5c9dcae1b20ecb35d592a8
TaeGeun Jang <devmeeple@gmail.com>
2025-04-23 17:42:02 +0900

    [5-2] 코틀린에서 다양한 함수를 다루는 방법

---
+++ src/main/java/starter/lec16/Lec16Main.java
@@ -0,0 +1,12 @@
+package starter.lec16;
+
+public class Lec16Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        int number = 100;
+
+        // 확장 함수
+        StringUtilsKt.lastChar("ABC");
+    }
+}
---
+++ src/main/kotlin/starter/lec16/StringUtils.kt
@@ -0,0 +1,10 @@
+package starter.lec16
+
+fun main() {
+    val str = "ABC"
+    println(str.lastChar())
+}
+
+fun String.lastChar(): Char {
+    return this[this.length - 1]
+}
